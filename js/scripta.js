b1990d8683b72c4611d11a33e6770893fa0509aa
raquel <raquel.aguilar@grupojaque.com>
2017-06-16 23:56:17 -0500

    changing format of variables and adding graphs

--- js/scripta.js
+++ js/scripta.js
@@ -1,10 +1,8 @@
 var w1 = d3.select("#mapa").style("width");
 var width = parseInt(w1,10);
 var height = width * 0.65;
-
-console.log(width);
-
 var mapad3 = d3.map();
+var windowHeight = $( window ).height();
 
 var proy = d3.geo.mercator()
 			.center([-102,23.7])
@@ -14,185 +12,83 @@ var proy = d3.geo.mercator()
 var camino = d3.geo.path()
 			.projection(proy);
 
-var svg = d3.select("#d2").append("svg")
-			.attr("width",width)
-			.attr("height",height);
-
 var svg2 = d3.select("#mapa").append("svg")
 			.attr("width",width)
 			.attr("height",height)
 			.append("g").attr("class","mapa");
 
-var scalax = d3.scale.ordinal().rangeRoundBands([0,width],0.1);
-var scalay = d3.scale.linear().range([height*0.5,0]).domain([0,100]);
+var scalax = d3.scale.ordinal().rangeRoundBands([0,200],0.1);
+var scalay = d3.scale.linear().range([windowHeight*(2/3)/9,0]).domain([0,50000]);
 var scalac = d3.scale.linear().range(["#ffffff","#6600b7"]).domain([0,30000]);
 
 d3.json("./datos/edos.json",function(error,data){
-d3.json("./datos/estmun2.json",function(error,poligonos){
+	d3.json("./datos/p28op1.json",function(error,p28op1){
+		d3.json("./datos/p28op2.json",function(error,p28op2){
+			d3.json("./datos/p28op3.json",function(error,p28op3){
+				d3.json("./datos/p28op4.json",function(error,p28op4){
+					d3.json("./datos/p28op5.json",function(error,p28op5){
+							d3.json("./datos/estmun2.json",function(error,poligonos){
+								var estadospoli = topojson.feature(poligonos, poligonos.objects.estados);
+
+								estadospoli.features.forEach(function(ele){
+									if(ele.id === "Coahuila de Zaragoza"){
+										ele.id = "Coahuila"
+									};
+									if(ele.id === "Michoacán de Ocampo"){
+										ele.id = "Michoacán"
+									};
+									if(ele.id === "Veracruz de Ignacio de la Llave"){
+										ele.id = "Veracruz"
+									};
+								})
+
+								svg2.selectAll("poligonos")
+									.data(estadospoli.features)
+									.enter().append("path")
+									.attr("class","estado")
+									.attr("d",camino)
+									.style("fill","white")
+									.style("stroke","white")
+									.style("stroke-width",0.5)
+
+								var ndata = data;
+								
+								d3.selectAll(".estado")
+									.on("click",mujeres)
+									.style("stroke","black")
+									.style("stroke-width",0.5)
+									.style("fill",function(d,i){
+										var temporal = ndata[i].MujeresSI;
+										return scalac(temporal)});
 
 
-	var estadospoli = topojson.feature(poligonos, poligonos.objects.estados);
-
-	estadospoli.features.forEach(function(ele){
-		if(ele.id === "Coahuila de Zaragoza"){
-			ele.id = "Coahuila"
-		};
-		if(ele.id === "Michoacán de Ocampo"){
-			ele.id = "Michoacán"
-		};
-		if(ele.id === "Veracruz de Ignacio de la Llave"){
-			ele.id = "Veracruz"
-		};
+								function mujeres (d,i) {
+									if (ndata[i].MujeresSI == 0) {
+										$(".lightbox").addClass("active")
+										d3.select(".msj")
+										.append("span")
+										.html("En el estado de " + d.id + " no hay mujeres que quieran ser científicas o ingenieras, que tenga entre 18 y 20 años.");
+									} else{
+										$("#d2").addClass("active")
+										d3.select("#percent")
+											.html((ndata[i].MujeresSI*100)/ndata[i].TotalMujeres);
+										d3.select("#title")
+											.html(d.id);
+										
+										buildGraph("#op1",p28op1,i, "op1")
+										buildGraph("#op2",p28op2,i, "op2")
+										buildGraph("#op3",p28op3,i, "op3")
+										buildGraph("#op4",p28op4,i, "op4")
+										buildGraph("#op5",p28op5,i, "op5")
+										
+										};
+								}
+						})
+					})
+				})
+			})
+		})
 	})
-
-	svg2.selectAll("poligonos")
-		.data(estadospoli.features)
-		.enter().append("path")
-		.attr("class","estado")
-		.attr("d",camino)
-		.style("fill","white")
-		.style("stroke","white")
-		.style("stroke-width",0.5)
-
-	//console.log("edos",data[1])
-	var ndata = data;
-	console.log("ndata",ndata[1].MujeresSI)
-	// ndata.forEach(function(ele){
-	// 	mapad3.set(ele.edo,ele.mujeres)
-	// });
-	
-	d3.selectAll(".estado")
-		.on("click",mujeres)
-		.style("stroke","black")
-		.style("stroke-width",0.5)
-		.style("fill",function(d,i){
-			var temporal = ndata[i].MujeresSI;
-
-			// var temporal2 = ndata.filter(function(ele){return ele.edo === d.id});
-			// console.log("temporal2",temporal2)
-			// var temporal = temporal2[0].mujeres;
-
-			//var temporal = mapad3.get(d.id);
-			return scalac(temporal)});
-
-
-	function mujeres (d,i) {
-		if (ndata[i].MujeresSI == 0) {
-			$(".lightbox").addClass("active")
-			d3.select(".msj")
-			.append("span")
-			.html("En el estado de " + d.id + " no hay mujeres que quieran ser científicas o ingenieras, que tenga entre 18 y 20 años.");
-		} else{
-			$("#d2").addClass("active")
-			d3.select("#percent")
-				.html((ndata[i].MujeresSI*100)/ndata[i].TotalMujeres);
-			d3.select("#title")
-				.html(d.id);
-		};
-	}
-	
-	// var estados = data.map(function(ele){return ele.edo});
-	// console.log(estados);
-
-	// var nestados = estadospoli.features.map(function(ele){return ele.id});
-	// console.log(nestados);
-
-	// scalax.domain(estados);
-
-	/*
-	d3.select("svg").append("g").attr("class","barras")
-		.selectAll("rect")
-		.data(ndata).enter()
-		.append("rect")
-		.attr("class","barra")
-		.attr("x",function(d,i){return scalax(d.estado)})
-		.attr("y",function(d,i){return scalay(0)})
-		.attr("height",function(d,i){return scalay(0) - scalay(0)})
-		//.attr("y",function(d,i){return scalay(d.valor)})
-		//.attr("height",function(d,i){return scalay(0) - scalay(d.valor)})
-		.attr("width",scalax.rangeBand())
-		.style("fill",function(d,i){return scalac(d.valor)});
-
-	d3.select("svg").append("g").attr("class","etiquetas")
-		.selectAll("text")
-		.data(ndata).enter()
-		.append("g").attr("class","et1")
-		.append("text")
-		.attr("x",function(d,i){return scalax(d.estado) - scalax.rangeBand()*0.5})
-		.attr("y",function(d,i){return scalay(0) + scalax.rangeBand()*0.7})
-		.text(function(d,i){return d.estado})
-		.style("font-size",width/60 + "px")
-		.style("text-anchor","end")
-		.attr("transform",function(d,i){return "rotate(-90," + scalax(d.estado) + "," + scalay(0) + ")"});
-
-	d3.selectAll("rect").transition()
-		.duration(2000)
-		.attr("y",function(d,i){return scalay(d.valor)})
-		.attr("height",function(d,i){return scalay(0) - scalay(d.valor)});
-
-	d3.selectAll(".barra").on("mouseover",info);
-	d3.selectAll(".barra").on("mouseout",salir);
-
-	d3.selectAll(".estado").on("mouseover",info2);
-	d3.selectAll(".estado").on("mouseout",salir);
-
-	function info2(d,i){
-
-		d3.selectAll(".barra").classed("apagado",function(p,j){return p.estado !== d.id});
-		d3.selectAll(".estado").classed("apagado",function(p,j){return d !== p});
-
-		d3.selectAll(".apagado").style("opacity",0);
-
-		d3.select("#d1")
-			.append("h2")
-			.attr("class","info")
-			.html(d.id);
-
-		d3.select("#d1")
-			.append("p")
-			.attr("class","info")
-			.html("Pobreza extrema: ")
-			.append("span")
-			.html(mapad3.get(d.id) + "%")
-			.style("font-weight","bold");
-
-	}
-
-	function info(d,i){
-
-		d3.selectAll(".barra").classed("apagado",function(p,j){return p !== d});
-		d3.selectAll(".estado").classed("apagado",function(p,j){return d.estado !== p.id})
-
-		d3.selectAll(".apagado").style("opacity",0);
-
-		d3.select("#d1")
-			.append("h2")
-			.attr("class","info")
-			.html(d.estado);
-
-		d3.select("#d1")
-			.append("p")
-			.attr("class","info")
-			.html("Pobreza extrema: ")
-			.append("span")
-			.html(d.valor + "%")
-			.style("font-weight","bold");
-	}
-
-	 function salir(d,i){
-
-	 	d3.selectAll(".barra").transition().style("opacity",1);
-	 	d3.selectAll(".estado").transition().style("opacity",1);
-
-	 	d3.selectAll(".barra").classed("apagado",function(){return false});
-	 	d3.selectAll(".estado").classed("apagado",function(){return false});
-
-	 	d3.selectAll(".info").remove();
-
-	 }*/	
-
-
-})
 })
 
 
@@ -204,4 +100,41 @@ $('.close-button').click(function(){
 $('#back').click(function(){
 	$('#d2').removeClass('active');
 	$('#infoEdo span').empty();
+	$('.resp svg').remove();
 })
+
+function parseData (originalData, idEstado, idQuestion) {
+	var res=[];
+	var keys = Object.keys(originalData[idEstado][idQuestion][0]);
+	for(var i = 0; i < keys.length; i++){
+		var elemento = {}
+		var val = originalData[idEstado][idQuestion][0][keys[i]]
+		elemento["key"]="opcion"+keys[i]
+		elemento["val"]=val
+		res.push(elemento)
+	}
+	return res
+}
+
+function buildGraph (selector, rawData, index, idQuestion) {
+	var parsedData = parseData(rawData, index, idQuestion)
+	var domainX = parsedData.map(function(elem){return elem.key});
+	scalax.domain(domainX);
+	
+	d3.select(selector).append("svg")
+		.attr("width","200px")
+			.attr("height",windowHeight*(2/3)/7)
+		.append("g").attr("class","barras")
+		.selectAll("rect")
+		.data(parsedData).enter()
+		.append("rect")
+		.attr("class","barra")
+		.attr("x",function(d,i){return scalax(d.key)})
+		.attr("y",function(d,i){return scalay(d.val)})
+		//.attr("height",function(d,i){return scalay(0) - scalay(0)})
+		//.attr("y",function(d,i){return scalay(d.valor)})
+		.attr("height",function(d,i){return scalay(0) - scalay(d.val)})
+		.attr("width",scalax.rangeBand())
+		.style("fill",function(d,i){return scalac(d.val)});
+		
+}
